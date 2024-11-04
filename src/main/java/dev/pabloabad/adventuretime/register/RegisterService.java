890e1b493959b0fc1e39c6e7ce32516c8a93851f
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-04 18:24:53 +0100

    created Register service

---
+++ src/main/java/dev/pabloabad/adventuretime/register/RegisterService.java
@@ -0,0 +1,61 @@
+package dev.pabloabad.adventuretime.register;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import dev.pabloabad.adventuretime.facades.EncoderFacade;
+import dev.pabloabad.adventuretime.facades.implementations.IEncryptFacade;
+import dev.pabloabad.adventuretime.profiles.Profile;
+import dev.pabloabad.adventuretime.profiles.ProfileService;
+import dev.pabloabad.adventuretime.roles.Role;
+import dev.pabloabad.adventuretime.roles.RoleService;
+import dev.pabloabad.adventuretime.roles.exceptions.RoleNotFoundException;
+import dev.pabloabad.adventuretime.users.User;
+import dev.pabloabad.adventuretime.users.UserRepository;
+
+@Service
+public class RegisterService {
+
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final ProfileService profileService;
+    private final IEncryptFacade encoderFacade;
+
+    public RegisterService(UserRepository userRepository, RoleService roleService, ProfileService profileService,
+            EncoderFacade encoderFacade) {
+        this.userRepository = userRepository;
+        this.roleService = roleService;
+        this.profileService = profileService;
+        this.encoderFacade = encoderFacade;
+    }
+
+    public User save(RegisterDto newRegisterDto) {
+        String passwordDecoded = encoderFacade.decode("base64", newRegisterDto.getPassword());
+        String passwordEncoded = encoderFacade.encode("bcrypt", passwordDecoded);
+
+        User user = new User(newRegisterDto.getUsername(), passwordEncoded);
+        user.setRoles(assignDefaultRole());
+
+        User savedUser = userRepository.save(user);
+
+        Profile profile = new Profile(newRegisterDto.getEmail(), savedUser);
+        profileService.save(profile);
+
+        return savedUser;
+    }
+
+    public Set<Role> assignDefaultRole() {
+        Set<Role> roles = new HashSet<>();
+        try {
+            Role defaultRole = roleService.getById(1L);
+            roles.add(defaultRole);
+        } catch (RoleNotFoundException e) {
+            // Handle the exception, e.g., log it or rethrow it
+            e.printStackTrace();
+        }
+
+        return roles;
+    }
+}
