6369d4ffb50ee61a1ce0e0d5b6856f3c7b0ea219
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-04 18:04:29 +0100

    created Participants Dto

---
+++ src/main/java/dev/pabloabad/adventuretime/dtos/ParticipantDto.java
@@ -0,0 +1,16 @@
+package dev.pabloabad.adventuretime.dtos;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ParticipantDto {
+    
+    private Long id;
+    private LocalDateTime joinedAt;
+    private Long eventId;
+    private Long userId;
+}
