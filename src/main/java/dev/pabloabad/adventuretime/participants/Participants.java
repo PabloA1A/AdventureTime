3d4f5c3be303a8c09ce4aa4a3abde2389957e1ad
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-04 17:39:32 +0100

    created Participants model

---
+++ src/main/java/dev/pabloabad/adventuretime/participants/Participants.java
@@ -0,0 +1,36 @@
+package dev.pabloabad.adventuretime.participants;
+
+import java.time.LocalDateTime;
+
+import dev.pabloabad.adventuretime.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "participants")
+@Data
+@AllArgsConstructor
+@Builder
+public class Participants {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    private Long event_id;
+    @Column(nullable = false)
+    private Long user_id;
+    @Column(nullable = false)
+    private LocalDateTime joined_at;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Events event;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
