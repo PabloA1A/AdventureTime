e9781f7d0eafcd05dde9b1a36a93078981d765df
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-05 22:20:25 +0100

    created SubscribeDto

---
+++ src/main/java/dev/pabloabad/adventuretime/dtos/SubscribeDto.java
@@ -0,0 +1,11 @@
+package dev.pabloabad.adventuretime.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SubscribeDto {
+    
+    private Long register;
+}
